5cf5797c4279575e5299c00e7efc057e79d4a297
gabrielbrunheira <gabriel.brunheira@lnls.br>
2018-04-04 13:23:27 -0300

    Included buffers funcionalities

--- app/communication_drivers/common/structs.h
+++ app/communication_drivers/common/structs.h
@@ -24,7 +24,6 @@
 
 #include <stdint.h>
 
-
 typedef enum
 {
     Idle,
@@ -56,6 +55,75 @@ typedef union
 {
     uint8_t     u8[4];
     float       f;
-}  u_float_t;
+} u_float_t;
+
+/**
+ * Initialization for an instance of ```buf_t```. It requires a pre-defined
+ * ```float``` array, addressed by ```p_buf_start```
+ *
+ * @param p_buf pointer to buffer structure
+ * @param p_buf_start pointer to the first element of the pre-defined array
+ * @param size number of elements on the array used by the buffer
+ */
+extern void init_buffer(buf_t *p_buf, volatile float *p_buf_start,
+                        uint16_t size);
+
+/**
+ * Set values from buffer to 0 and reset index pointer
+ *
+ * @param p_buf pointer to buffer structure
+ */
+extern void reset_buffer(buf_t *p_buf);
+
+/**
+ * Enable specified buffer
+ *
+ * @param p_buf pointer to buffer structure
+ */
+extern void enable_buffer(buf_t *p_buf);
+/**
+ * Disable specified buffer
+ *
+ * @param p_buf pointer to buffer structure
+ */
+extern void disable_buffer(buf_t *p_buf);
+
+/**
+ * Return number of elements of specified buffer
+ *
+ * @param p_buf pointer to buffer structure
+ * @return buffer size
+ */
+extern uint16_t size_buffer(buf_t *p_buf);
+
+/**
+ * Return position of current index pointer on specified buffer
+ *
+ * @param p_buf pointer to buffer structure
+ * @return index position
+ */
+extern uint16_t idx_buffer(buf_t *p_buf);
+
+/**
+ * Insert new data to buffer. If buffer is enabled and full (```idx == end```),
+ * it wraps around to the beginning, and keeps inserting new values. If buffer
+ * is disabled, it stops to insert as soon as the buffer is filled.
+ *
+ * @param p_buf pointer to buffer structure
+ * @param data new data to insert to buffer
+ * @return indicate whether buffer is full
+ */
+extern uint16_t insert_buffer(buf_t *p_buf, float data);
+
+/**
+ * Test to indicate whether the buffer contains any sample outside the limits
+ * determined by the
+ *
+ * @param p_buf pointer to buffer structure
+ * @param value reference value for test
+ * @param tol tolerance value for test
+ * @return
+ */
+extern uint16_t test_buffer_limits(buf_t *p_buf, float value, float tol);
 
 #endif /* STRUCTS_H_ */
