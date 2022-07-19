e1ebd76cdda269c5106e5ee4fc65f449b51d7890
Gabriel Oehlmeyer Brunheira <gabrielbrunheira@gmail.com>
2022-07-19 17:01:02 -0300

    Creation of ps_module for resonant converter prototype for SWLS

--- main.c
+++ main.c
@@ -77,6 +77,7 @@
 #include "communication_drivers/ps_modules/fap_4p/fap_4p.h"
 #include "communication_drivers/ps_modules/fbp/fbp.h"
 #include "communication_drivers/ps_modules/fbp_dclink/fbp_dclink.h"
+#include "communication_drivers/ps_modules/resonant_swls/resonant_swls.h"
 
 extern unsigned long RamfuncsLoadStart;
 extern unsigned long RamfuncsRunStart;
@@ -212,6 +213,12 @@ int main(void) {
         case FAC_2P_DCDC_IMAS:
         {
             fac_2p_dcdc_imas_system_config();
+            break;
+        }
+
+        case RESONANT_SWLS:
+        {
+            resonant_swls_system_config();
             break;
         }
 
